659695cdd14e80deb9f47f2493aa9aa1d1719a61
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-12-27 17:41:58 +0700

    Add options: --pm, --line-prefix, --put-hook-at-the-end, --add-begin-block

--- lib/Module/FatPack.pm
+++ lib/Module/FatPack.pm
@@ -86,7 +86,31 @@ _
             schema => 'bool',
             default => 1,
         },
+        line_prefix => {
+            schema => ['str*', min_len => 1],
+            default => '  ',
+        },
+        put_hook_at_the_end => {
+            summary => 'Put the require hook at the end of @INC using "push" '.
+                'instead of at the front using "unshift"',
+            schema => ['bool*', is=>1],
+        },
+        add_begin_block => {
+            summary => 'Surround the code inside BEGIN { }',
+            schema => ['bool*'],
+        },
 
+        pm => {
+            summary => "Make code suitable to put inside .pm file instead of script",
+            schema => ['bool*', is=>1],
+            description => <<'_',
+
+This setting adjusts the code so it is suitable to put one or several instances
+of the code inside one or more .pm files. Also sets default for --line-prefix
+'#' --no-add-begin-block --put-hook-at-the-end.
+
+_
+        },
     },
     examples => [
         {
@@ -101,11 +125,18 @@ _
 sub fatpack_modules {
     my %args = @_;
 
+    my $pm = $args{pm};
+    my $line_prefix = $args{line_prefix} // $pm ? '#':'  ';
+    my $add_begin_block = $args{add_begin_block} // $pm ? 0:1;
+    my $put_hook_at_the_end = $args{put_hook_at_the_end} // $pm ? 1:0;
+
     my %module_srcs; # key: mod_pm
+    my %fatpack_keys;
     if ($args{module_srcs}) {
         for my $mod (keys %{ $args{module_srcs} }) {
             my $mod_pm = $mod; $mod_pm =~ s!::!/!g; $mod_pm .= ".pm" unless $mod_pm =~ /\.pm\z/;
             $module_srcs{$mod_pm} = $args{module_srcs}{$mod};
+            $fatpack_keys{$mod_pm}++;
         }
     } else {
         require Module::Path::More;
@@ -120,6 +151,7 @@ sub fatpack_modules {
                 open my($fh), "<", $path or die "Can't open $path: $!";
                 ~~<$fh>;
             };
+            $fatpack_keys{$mod_pm}++;
         }
     }
 
@@ -140,55 +172,74 @@ sub fatpack_modules {
     my @res;
 
     push @res, $args{preamble} if defined $args{preamble};
-    push @res, 'BEGIN {', "\n";
-    push @res, 'my %fatpacked;', "\n\n";
+    if ($add_begin_block) {
+        push @res, 'BEGIN {', "\n";
+    } else {
+        push @res, "# BEGIN FATPACK CODE\n{\n";
+    }
+    push @res, <<'_' if $args{assume_strict} // 1;
+    no strict 'refs';
+_
     for my $mod_pm (sort keys %module_srcs) {
         my $label = uc($mod_pm); $label =~ s/\W+/_/g; $label =~ s/\_PM$//;
-        push @res, '$fatpacked{"', $mod_pm, q|"} = '#line '.(1+__LINE__).' "'.__FILE__."\"\n".<<'|, $label, "';\n";
-        $module_srcs{$mod_pm} =~ s/^/  /gm;
+        push @res, '    $main::fatpacked{"', $mod_pm, '"} = \'' . $line_prefix . q|#line '.(1+__LINE__).' "'.__FILE__."\"\n".<<'|, $label, "';\n";
+        $module_srcs{$mod_pm} =~ s/^/$line_prefix/gm;
         push @res, $module_srcs{$mod_pm};
         push @res, "$label\n\n";
     }
-    push @res, <<'_';
-s/^  //mg for values %fatpacked;
-
-my $class = 'FatPacked::'.(0+\%fatpacked);
-_
-
-    push @res, <<'_' if $args{assume_strict} // 1;
-no strict 'refs';
-_
-    push @res, <<'_';
-*{"${class}::files"} = sub { keys %{$_[0]} };
-
-if ($] < 5.008) {
-  *{"${class}::INC"} = sub {
-     if (my $fat = $_[0]{$_[1]}) {
-       return sub {
-         return 0 unless length $fat;
-         $fat =~ s/^([^\n]*\n?)//;
-         $_ = $1;
-         return 1;
-       };
-     }
-     return;
-  };
-}
-
-else {
-  *{"${class}::INC"} = sub {
-    if (my $fat = $_[0]{$_[1]}) {
-      open my $fh, '<', \$fat
-        or die "FatPacker error loading $_[1] (could be a perl installation issue?)";
-      return $fh;
+    if ($pm) {
+        push @res, '    $main::fatpacked{$_} =~ s/^'.quotemeta($line_prefix).'//mg for ('.join(", ", map {"'$_'"} sort keys %fatpack_keys).');'."\n";
+    } else {
+        push @res, '    s/^'.quotemeta($line_prefix).'//mg for values %main::fatpacked;'."\n";
     }
-    return;
-  };
-}
-
-unshift @INC, bless \%fatpacked, $class;
-  } # END OF FATPACK CODE
+    push @res, <<'_';
+    my $class = 'FatPacked::'.(0+\%main::fatpacked);
 _
+
+    # unneeded?
+#    push @res, <<'_';
+#    *{"${class}::files"} = sub { keys %{$_[0]} };
+#_
+
+    my $hook_src = <<'_';
+    if ($] < 5.008) {
+        *{"${class}::INC"} = sub {
+            if (my $fat = $_[0]{$_[1]}) {
+                return sub {
+                    return 0 unless length $fat;
+                    $fat =~ s/^([^\n]*\n?)//;
+                    $_ = $1;
+                    return 1;
+                };
+            }
+            return;
+        };
+    } else {
+        *{"${class}::INC"} = sub {
+            if (my $fat = $_[0]{$_[1]}) {
+                open my $fh, '<', \$fat
+                    or die "FatPacker error loading $_[1] (could be a perl installation issue?)";
+                return $fh;
+            }
+            return;
+        };
+    }
+_
+    if ($pm) { $hook_src =~ s/\R\s+/ /g }
+    push @res, $hook_src;
+    push @res, <<'_';
+    my $hook = bless(\%main::fatpacked, $class);
+_
+    if ($put_hook_at_the_end) {
+        push @res, <<'_';
+    push @INC, $hook unless grep {ref($_) && "$_" eq "$hook"} @INC;
+_
+    } else {
+        push @res, <<'_';
+    unshift @INC, $hook unless grep {ref($_) && "$_" eq "$hook"} @INC;
+_
+    }
+    push @res, "} # END OF FATPACK CODE\n\n";
     push @res, $args{postamble} if defined $args{postamble};
 
     if ($args{output}) {
